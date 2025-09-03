d3d6cb35edf13cfa1c6bee10db4b3096f882551d
Daulet Kydrali <dakacom@gmail.com>
2025-09-03 16:18:08 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const sheetUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSFLGLmPWBE5wyGkDp49_K6eqZekLeOzPvXwjJS7BYvxrUkxRmu2Wn4LN8t6pKggz3BptK-R1av6xh6/pub?output=tsv';
+
+const sheetUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSvFYhvFjLPFVbVQzVEhLsWJ_JHkacNR54z6pRmLDZFlrW8sSt6dyAaW0-PulDkef3gAMGlEyFrb4p7/pub?output=tsv';
 
 async function loadTSVData() {
   const res = await fetch(sheetUrl);
@@ -17,23 +18,22 @@ async function loadTSVData() {
 
 function renderAnimations(data) {
   const container = document.getElementById('catalog');
-  data.forEach((item, index) => {
+  data.forEach((item) => {
     const card = document.createElement('div');
     card.className = 'card';
     card.innerHTML = `
-      <img src="${item.thumbnail}" class="preview" alt="preview">
-      <div class="lottie-player" id="lottie-${index}"></div>
+      <lottie-player
+        src="${item.url}"
+        background="transparent"
+        speed="1"
+        loop
+        hover
+        autoplay
+        style="width: 100%; height: 300px;">
+      </lottie-player>
       <div class="title">${item.name}</div>
     `;
     container.appendChild(card);
-
-    lottie.loadAnimation({
-      container: document.getElementById(`lottie-${index}`),
-      renderer: 'svg',
-      loop: true,
-      autoplay: false,
-      path: item.url
-    });
   });
 }
 
